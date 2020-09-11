2606b6b34ff4c1a4e46af8bbbb547a11794ac564
RYANSUTODIWIRYO <ryan.s.sutodiwiryo@gmail.com>
2020-09-11 08:49:19 +0700

    update logout

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { Login, Home } from "./screen"
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { View, Button } from 'react-native'
 
 const Stack = createStackNavigator()
 
@@ -10,19 +11,33 @@ class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">          
+        <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={({route, navigation}) => ({
+          headerRight: () => (
+            <View style={{marginRight: 10}}>
+              <Button
+                // only alert is ok, the other is error.
+                onPress={() => navigation.navigate('Login')}
+                title="Logout"
+                color="red"
+              />
+            </View>
+          )
+        })}>          
           <Stack.Screen
             name="Login"
             component={Login}
             options={{
               headerShown: null
+              
             }}  
           />
           <Stack.Screen
             name="Home"
-            component={Home}
+            component={Home}            
             options={{
-              headerShown: null
+              headerLeft: null,
             }}
           />
         </Stack.Navigator>
